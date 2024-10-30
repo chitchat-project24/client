3ede4efc81fa5482405a5682913595f2c95cd748
이선우 <susan@Susans-Mac.local>
2024-10-30 16:08:10 +0900

    chore: z-index and remove scss import

--- src/pages/ProfileListPage/ProfileListPage.tsx
+++ src/pages/ProfileListPage/ProfileListPage.tsx
@@ -1,12 +1,5 @@
 import { NavBar } from '../../components';
-import styles from './ProfileList.module.scss';
-import cs from 'classnames/bind';
-const cx = cs.bind(styles);
 
 export default function ProfileListPage() {
-  return (
-    <div className={cx('wrapper')}>
-      <NavBar />
-    </div>
-  );
+  return <NavBar />;
 }
