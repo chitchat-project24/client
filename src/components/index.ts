64c3dd946a0589e2911369eeb3f8be5ff8df0015
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-12-14 18:03:31 +0900

    ✨ Feat: login ui

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from './Header';
 export * from './List';
 export * from './ListItem';
 export * from './Footer';
+export * from './UserFormInput';
+export * from './LoginButton';
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,26 @@
+import styles from './LoginPage.module.scss';
+import cs from 'classnames/bind';
+import { images } from '../../assets/images';
+import { UserFormInput, LoginButton } from '../../components/index';
+const cx = cs.bind(styles);
+
 export default function LoginPage() {
-  return <></>;
+  return (
+    <div className={cx('login-container')}>
+      <div className={cx('logo')}>
+        <img src={images.Logo} />
+      </div>
+      <div className={cx('input-section')}>
+        <UserFormInput type="id" />
+        <UserFormInput type="password" />
+      </div>
+      <div className={cx('button-section')}>
+        <LoginButton text="로그인" />
+        <div className={cx('links')}>
+          <a>비밀번호 찾기</a>
+          <a>회원가입</a>
+        </div>
+      </div>
+    </div>
+  );
 }
